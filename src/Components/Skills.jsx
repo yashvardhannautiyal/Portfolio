264dd383caae97ebebcbe50e6cc98a17f2bb93ba
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2025-01-29 19:31:46 +0530

    gap modified

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -6,11 +6,11 @@ function Skills() {
       <div className="text-white font-bold text-4xl sm:text-6xl md:text-8xl flex justify-center">
         <p>SKILLS</p>
       </div>
-      <div className="flex flex-wrap justify-center gap-4 sm:gap-6 mt-10 sm:mt-14">
+      <div className="flex flex-wrap justify-center gap-4 md:gap-10 px-1 sm:px-6 mt-10 sm:mt-14">
         {SkillsList.map((skill, index) => (
           <div
             key={index}
-            className="px-2 sm:px-4 py-1 sm:py-2 rounded-xl flex items-center sm:space-x-7 border-3 border-color hover:cursor-pointer hover:border-orange-600"
+            className="px-2 sm:px-4 py-1 sm:py-2 rounded-xl flex items-center justify-center sm:space-x-3 border-3 border-color hover:cursor-pointer hover:border-orange-600"
           >
             <img
               src={skill.image}
