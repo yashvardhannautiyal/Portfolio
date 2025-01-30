0c8a307be3ccd91c93dc2bfaffae06e80bd5d3e5
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2025-01-31 00:09:22 +0530

    contact edited

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,3 +1,6 @@
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
 function Contact(){
     return(
         <div className="projects-container mt-16">
@@ -5,7 +8,14 @@ function Contact(){
             <p className="text-white flex justify-center">LET'S WORK</p>
             <p className="font-color1 flex justify-center">TOGETHER</p>
         </div>
-        
+        <div className = "logocontainer text-orange-600 flex justify-center mt-14">
+        <div className="flex py-3 gap-8 text-3xl font-extrabold">
+        <FaXTwitter /> 
+        <FaLinkedinIn />
+        <IoMail />
+
+        </div>
+        </div>
         </div>
     )
 }
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -1,38 +1,33 @@
-import { FaXTwitter } from "react-icons/fa6";
-import { FaLinkedinIn } from "react-icons/fa";
-import { IoMail } from "react-icons/io5";
+
 
 function HeroSection() {
-    return(
-        <>
-        <div className="main-container px-2 sm:px-18 pt-14 font-white">   
-            <div className="">    
-        <div className = "about-container ">
-        <p className="text-normal text-lg sm:text-2xl md:text-2xl lg:text-3xl font-bold text-orange-600">YASH VARDHAN NAUTIYAL</p>
-        <div className="text-6xl sm:text-6xl md:text-8xl font-bold">
-        <p className="text-white">WEBSITE</p>
-        <p className="font-color1">DEVELOPER</p>
-        </div>
-        </div>
-        <p className="font-color2 pt-2 w-full sm-w-xl md:w-3xl font-medium text-normal md:text-lg">
-            <span className="text-white">Passionate about creating intuitive and engaging user experiences.</span>I'm a website developer and designer based in <span className="text-white">India</span>, passionate about delivering high-quality websites and applications.</p>
-        </div>
-
-        
-        </div>
-{/* 
-        <div className = "logocontainer font-color2">
-        <div className="flex font-color2 py-3 space-x-5 text-2xl font-extrabold">
-        <FaXTwitter /> 
-        <FaLinkedinIn />
-        <IoMail />
-
+  return (
+    <>
+      <div className="main-container px-2 sm:px-18 pt-14 font-white flex items-end gap-36">
+        <div className="about-container">
+          <div className="name-container">
+            <p className="text-normal text-lg sm:text-2xl md:text-2xl lg:text-3xl font-bold text-orange-600">
+              YASH VARDHAN NAUTIYAL
+            </p>
+            <div className="text-6xl sm:text-6xl md:text-8xl font-bold">
+              <p className="text-white">WEBSITE</p>
+              <p className="font-color1">DEVELOPER</p>
+            </div>
+          </div>
+          <p className="font-color2 pt-2 w-full sm-w-xl md:w-3xl font-medium text-normal md:text-lg">
+            <span className="text-white">
+              Passionate about creating intuitive and engaging user experiences.
+            </span>
+            I'm a website developer and designer based in{" "}
+            <span className="text-white">India</span>, passionate about
+            delivering high-quality websites and applications.
+          </p>
         </div>
 
-            
-        </div> */}
-        </>
-    )
+      </div>
+      
+    </>
+  );
 }
 
 export default HeroSection;
