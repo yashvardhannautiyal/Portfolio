ed3bd76996f5084bbbe40d522c3b8a5340623106
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2025-03-18 00:04:07 +0530

    github project added

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -10,6 +10,16 @@ const ProjectList = [
   },
   {
     id: 2,
+    name: "Github User Finder",
+    description:
+      "GitHub User Finder is a web app that allows users to search for GitHub profiles quickly and view detailed information about them. It features a user-friendly interface with a dark mode option.",
+    image: "../project_img/githubFinder.png",
+    stack: "ReactJs, Context API, JavaScript, TailwindCss",
+    gitLink: "https://github.com/yashvardhannautiyal/Github-user-finder",
+    liveLink: "https://github-user-finder-dun-five.vercel.app/",
+  },
+  {
+    id: 3,
     name: "ToDoEase",
     description:
       "ToDoEase is a simple and efficient to-do app allowing users to add, complete, and delete task with the current date and time for reference.",
@@ -19,7 +29,7 @@ const ProjectList = [
     liveLink: "https://to-do-app-two-sepia.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "MyDictionary App",
     description:
     "MyDictionary app with feature of dark mode fetches data from dictionary API and provides meaning with their examples to the user.",
@@ -29,7 +39,7 @@ const ProjectList = [
     liveLink: "https://my-dictionary-jade.vercel.app/",
   },
   {
-    id: 4,
+    id: 5,
     name: "Weather App",
     description:
     "Weather app showing current weather conditions of the given location with current date and current time.",
@@ -39,7 +49,7 @@ const ProjectList = [
     liveLink: "https://weather-app-one-iota-57.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     name: "BharatVoyage",
     description:
       "BharatVoyage is a landing page built with using Tailwind utility classes including dynamic and adaptive gallery.",
@@ -49,7 +59,7 @@ const ProjectList = [
     liveLink: "https://landing-page-zeta-ochre-70.vercel.app/",
   },
   {
-    id: 6,
+    id: 7,
     name: "Calculator",
     description:
     "Simple calculator app performing basic mathematic operations.",
@@ -59,7 +69,7 @@ const ProjectList = [
     liveLink: "https://yashvardhannautiyal.github.io/Calculator/",
   },
   {
-    id: 7,
+    id: 8,
     name: "Guess the Number Game",
     description:
       "Number guessing game where the user is scored according to the number of guesses he made.",
